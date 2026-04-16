f8428ef5d3d2e209ecf0b342aa97b64588dfe9c6
Christian Chiarulli <chris.machine@pm.me>
2026-04-16 00:40:03 -0400

    support rust

--- lua/plugins/nvim-treesitter.lua
+++ lua/plugins/nvim-treesitter.lua
@@ -1,5 +1,11 @@
 require("nvim-treesitter.config").setup()
 
+-- Ensure parsers are installed (idempotent - skips if already present)
+require("nvim-treesitter").install({
+  "rust",
+  "toml",
+})
+
 -- In Neovim 0.12, treesitter highlight is managed natively
 -- Enable it for all buffers with an available parser
 vim.api.nvim_create_autocmd("FileType", {
